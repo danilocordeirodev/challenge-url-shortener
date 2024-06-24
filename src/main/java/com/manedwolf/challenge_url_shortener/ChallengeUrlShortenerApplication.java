304536061e4bf9a2fe6890fde2c0554b4e37d9c2
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-06-24 09:58:52 -0300

    Initial commit

---
+++ src/main/java/com/manedwolf/challenge_url_shortener/ChallengeUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.manedwolf.challenge_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manedwolf/challenge_url_shortener/ChallengeUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manedwolf.challenge_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
